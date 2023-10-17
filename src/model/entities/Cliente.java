bec9f0f3c9b8b267536c265a5c82527ce6d27a4e
Gabriel Costa <woutogamer51@gmail.com>
2023-10-17 15:44:22 -0300

    cliente da uma olhada oq falta

--- src/model/entities/Cliente.java
+++ src/model/entities/Cliente.java
@@ -1,4 +1,58 @@
 package model.entities;
 
 public class Cliente {
+
+
+
+    private int clienteid;
+    private String name;
+
+    private String email;
+
+    private String cpf;
+
+    public cliente(int clienteid, String name, String email, String cpf) {
+        this.clienteid = clienteid;
+        this.name = name;
+        this.email = email;
+        this.cpf= cpf;
+    }
+
+    public int getClienteid() {
+        return clienteid;
+    }
+
+    public void setClienteid(int clienteid) {
+        this.clienteid = clienteid;
+    }
+
+    public void setId(clienteid) {
+        this.clienteid = clienteid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 }
+
+
