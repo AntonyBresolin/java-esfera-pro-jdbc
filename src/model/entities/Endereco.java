b534aab761f706452134fe6b7b215afb9796c0b0
Lucas Dreveck <lucasdrevc@outlook.com>
2023-10-19 15:13:07 -0300

    Create Endereco.java

--- src/model/entities/Endereco.java
+++ src/model/entities/Endereco.java
@@ -1,4 +1,90 @@
 package model.entities;
 
 public class Endereco {
+    private int id_Endereco;
+    private String cep;
+    private String estado;
+    private String cidade;
+    private String bairro;
+    private String rua;
+    private String numero;
+    private String complemento;
+
+    public Endereco() {
+    }
+
+    public Endereco(int id_Endereco, String cep, String estado, String cidade, String bairro, String rua, String numero, String complemento) {
+        this.id_Endereco = id_Endereco;
+        this.cep = cep;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.bairro = bairro;
+        this.rua = rua;
+        this.numero = numero;
+        this.complemento = complemento;
+    }
+
+    public int getId_Endereco() {
+        return id_Endereco;
+    }
+
+    public void setId_Endereco(int id_Endereco) {
+        this.id_Endereco = id_Endereco;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
 }
